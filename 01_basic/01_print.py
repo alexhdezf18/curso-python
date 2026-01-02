4ba18c4ed0af9daaabc1aabcc61abec058f942b4
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-01 23:18:34 -0600

    Aprendiendo a utilizar el print

---
+++ 01_basic/01_print.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print('Esto tambien funciona con una comilla simple')
+
+
+# Separador de argumentos por defecto es un espacio
+print("Python", "es", "genial")
+
+# Separador de argumentos por guión definido por el argumento sep
+print("Python", "es", "brutal", sep = "-")
+
+# End por defecto es un salto de linea, se puede cambiar con el argumento end
+print("Esto se imprime", end = " ")
+print("es una linea")
