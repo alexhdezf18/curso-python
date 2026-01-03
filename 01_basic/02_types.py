05d156d2bc87642f6a152c664a90fcb717e14891
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-02 22:33:25 -0600

    Aprendiendo los tipos de datos

---
+++ 01_basic/02_types.py
@@ -0,0 +1,33 @@
+# Python tiene varios tipo de datos
+# int: enteros
+# float: flotantes
+# str: cadenas de texto
+# bool: booleanos
+# list: listas
+# tuple: tuplas
+# dict: diccionarios
+# set: conjuntos
+# None: None
+
+"""
+Asi puedes hacer una cadena de texto multilinea para documentar tu codigo
+"""
+
+print(type(10))
+print(type(-5))
+print(type(5234523452412343130))
+
+
+# float: Numero con decimales
+print(type(10.5))
+print(type(-5.5))
+print(type(1e3))
+
+# Ausencia de valor
+print(type(None))
+
+# Numeros complejos
+print(type(1j))
+print(type(1+2j))
+print(type(1-2j))
+print(type(1+2j)) 
