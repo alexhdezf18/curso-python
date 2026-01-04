1dc63813b3a87b9041376fd806583771311ab5eb
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-03 23:27:35 -0600

    Aprendiendo condicionales

---
+++ 02_flow_control/01_if.py
@@ -0,0 +1,40 @@
+import os
+# Cada vez que ejecutamos el archivo se limpia la terminal
+os.system("clear")
+
+# Permite ejecutar ciertos bloques de codigo si se cumplen ciertas condiciones
+
+print("\n Sentencia simple condicional")
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad")
+
+
+print("\n Sentencia condicional con else")
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+print("\n Sentencia condicional con elif")
+nota = 7
+
+if nota >= 9:
+    print("Sobresaliente")
+elif nota >= 7:
+    print("Notable")
+elif nota >= 5:
+    print("Aprobado")
+else:
+    print("No esta calificado")
+
+
+print("\n Condiciones multiples")
+edad = 25
+tiene_carnet = True
+
+if edad >= 18 and tiene_carnet:
+    print("Puedes conducir")
+else:
+    print("Policia")
