3eb99808d83601b5d1e107198dd5e363d46af3f6
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-04 22:33:38 -0600

    Aprendiendo listas

---
+++ 02_flow_control/03_list.py
@@ -0,0 +1,23 @@
+# Secuencias mutables de elementos
+# Pueden contener elementos de diferentes tipos
+
+
+# Creacion de listas
+print("\nCrear listas")
+lista1 = [1, 2, 3, 4, 5] # lista de enteros
+lista2 = ["manzanas", "peras", "platanos"] # lista de cadenas
+lista3 = [1, "hola", 3.14, True] # lista de tipos mixtos
+
+lista_vacia = []
+lista_de_listas = [[1, 2], [3, 4]]
+matrix = [[1, 2], [3, 4], [5, 6]]
+
+
+# Acceso a elementos por indice
+print("\nAcceso a elementos por indice")
+print(lista2[0]) # manzanas
+print(lista2[1]) # peras
+print(lista2[-1]) # platanos
+
+
+print(lista_de_listas[1][0])
