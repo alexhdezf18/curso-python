f6246bafc9a30fa7661538401068f079b548f937
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-11 22:40:50 -0600

    Terminando de resolver el tercer problema

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -9,3 +9,15 @@ goal = 8
 find_first_sum(nums, goal)  # [2, 3]
 """
 
+def find_first_sum(nums, goal):
+    for i in nums:
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == goal:
+                return [i, j]
+    return None # No se encontro ninguna combinacion
+
+
+nums = [4, 5, 6, 2]
+goal = 8
+result = find_first_sum(nums, goal) 
+print(result)
