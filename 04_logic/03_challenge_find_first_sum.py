848f6b67e858a686481047377a1b5aa844875b61
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-11 22:40:19 -0600

    Resolviendo el tercer problema

---
+++ 04_logic/03_challenge_find_first_sum.py
@@ -0,0 +1,11 @@
+f"""
+Dado un array de numero y un numero goal, encuentra los dos primeros numeros del
+array que sumen el numero goal y devuelve sus indices. Si no existe tal combinacion,
+devuelve None.
+
+nums = [4, 5, 6, 2]
+goal = 8
+
+find_first_sum(nums, goal)  # [2, 3]
+"""
+
